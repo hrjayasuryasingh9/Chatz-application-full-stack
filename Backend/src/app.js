bfb832716255f8a69b4a2feeab8c6794d105220a
H.R. JAYA SURYA SINGH <136779852+hrjayasuryasingh9@users.noreply.github.com>
2025-03-24 22:13:40 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -26,7 +26,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(express.json({ limit: "50mb" }));
 app.use(
   cors({
-    origin:"*",
+    origin:["https://chatz-application-full-stack.onrender.com","http://localhost:5173],
     credentials: true,
   })
 );
